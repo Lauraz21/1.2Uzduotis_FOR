0e1c87fda426383be03e277f60eea78f012e53d2
Laura <Laura@DESKTOP-BESSB6E>
2023-09-13 18:57:23 +0300

    Add project files.

---
+++ 1.2Uzduotis_FOR/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _1._2Uzduotis_FOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            double mult = 0;
+
+            Console.WriteLine($"Atsakymas: ");
+
+            for (int i = 1; i <= n; i++)
+            {
+                mult = Math.Pow(i,2);
+               
+                Console.WriteLine(mult);
+            }
+        }
+    }
+}
